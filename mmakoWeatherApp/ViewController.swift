f1aaee3e0f56e8e6af3c33411dd3837871896cd2
Mmako Sekhosana <mmako@glucode.com>
2023-05-21 12:44:00 +0200

    Initial Commit

---
+++ mmakoWeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mmakoWeatherApp
+//
+//  Created by Mmako Sekhosana on 2023/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
